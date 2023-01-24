72776426008ea5663bb576e71e33695bd2c9de0e
Rivilart <113963182+Rivilart@users.noreply.github.com>
2023-01-24 22:35:17 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,25 @@
+let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
 
- function toggleClass(element, class1, class2) {
-    if (element.classList.contains(class1)) {
-      element.classList.replace(class1, class2);
-    } else {
-      element.classList.replace(class2, class1);
-    }
-};
-   // Функция для переключения темы
- function toggleTheme() {
-    var body = document.querySelector("body");
-    body.classList.toggle("white");
-    var article = document.querySelectorAll("article");
-    for (var i = 0; i < article.length; i++) {
-       article[i].classList.toggle("whitearticle");
-    };
-    var navblock1 = document.querySelector("#navblock-1");
-    toggleClass(navblock1, "navblock-1", "whitenavblock1");
-    var navblock2 = document.querySelector("#navblock-2");
-    toggleClass(navblock2, "navblock-2", "whitenavblock2");
-    var topic = document.querySelectorAll("#topic");
-    for (var i = 0; i < topic.length; i++) {
-        toggleClass(topic[i], "topic", "whitetopic");
-     };
-     for (var i = 0; i < topic.length; i++) {
-        toggleClass(topic[i], "active", "whiteactive");
-     };
-    var home = document.querySelector("#home");
-    toggleClass(home, "home", "whitehome");
-    var up = document.querySelector("#up");
-    toggleClass(up, "up", "whiteup");
-    var span = document.querySelectorAll("span");
-    for (var i = 0; i < span.length; i++) {
-        toggleClass(span[i], "code", "whitecode");
-     };
-};
-// Навешиваем обработчик события на кнопку
-var toggleThemeButton = document.querySelector("#toggle-theme-button");
-toggleThemeButton.addEventListener("click", toggleTheme);
+changeThemeButtons.forEach(button => {
+    button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
+        let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
+        applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
+    });
+});
+
+function applyTheme(themeName) {
+    document.querySelector('[title="theme"]').setAttribute('href', `styles/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
+    changeThemeButtons.forEach(button => {
+        button.style.display = 'block'; // Показываем все кнопки смены темы
+    });
+    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
+    localStorage.setItem('theme', themeName);
+}
+
+let activeTheme = localStorage.getItem('theme'); // Проверяем есть ли в LocalStorage записаное значение для 'theme' и присваиваем его переменной.
+
+if(activeTheme === null || activeTheme === 'light') { // Если значение не записано, или оно равно 'light' - применяем светлую тему
+    applyTheme('light');
+} else if (activeTheme === 'dark') { // Если значение равно 'dark' - применяем темную
+    applyTheme('dark');
+}
