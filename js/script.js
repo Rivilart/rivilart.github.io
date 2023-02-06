a8d2c188292480c53e9d9a34d9ba8751611b9b52
Rivilart <113963182+Rivilart@users.noreply.github.com>
2023-02-06 20:59:53 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-let changeThemeButtons = document.querySelectorAll('.changeTheme'); // Помещаем кнопки смены темы в переменную
+let changeThemeButtons = document.querySelectorAll('.changeTheme');
 
 changeThemeButtons.forEach(button => {
-    button.addEventListener('click', function () { // К каждой добавляем обработчик событий на клик
-        let theme = this.dataset.theme; // Помещаем в переменную название темы из атрибута data-theme
-        applyTheme(theme); // Вызываем функцию, которая меняет тему и передаем в нее её название
+    button.addEventListener('click', function () { // К каждой добавляется обработчик событий на клик
+        let theme = this.dataset.theme; // Помещается в переменную название темы из атрибута data-theme
+        applyTheme(theme); // Вызывается функция, которая меняет тему и передается в нее её название
     });
 });
 
 function applyTheme(themeName) {
-    document.querySelector('[title="theme"]').setAttribute('href', `styles/theme-${themeName}.css`); // Помещаем путь к файлу темы в пустой link в head
+    document.querySelector('[title="theme"]').setAttribute('href', `styles/theme-${themeName}.css`);
     changeThemeButtons.forEach(button => {
-        button.style.display = 'block'; // Показываем все кнопки смены темы
+        button.style.display = 'block'; // Показывются все кнопки смены темы
     });
-    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрываем кнопку для активной темы
+    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none'; // Но скрывается кнопка для активной темы
     localStorage.setItem('theme', themeName);
 }
 
-let activeTheme = localStorage.getItem('theme'); // Проверяем есть ли в LocalStorage записаное значение для 'theme' и присваиваем его переменной.
+let activeTheme = localStorage.getItem('theme'); // Проверка есть ли в LocalStorage записаное значение для 'theme' и присваевание его переменной.
 
-if(activeTheme === null || activeTheme === 'light') { // Если значение не записано, или оно равно 'light' - применяем светлую тему
+if(activeTheme === null || activeTheme === 'light') { // Если значение не записано, или оно равно 'light' - применяется светлая тема
     applyTheme('light');
-} else if (activeTheme === 'dark') { // Если значение равно 'dark' - применяем темную
+} else if (activeTheme === 'dark') { // Если значение равно 'dark' - применяется темная
     applyTheme('dark');
 }
